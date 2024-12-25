94cbfee2778f769502fb5978b79db8c0eff1b650
Nirzi <nirzzii@mail.ru>
2024-12-25 12:31:00 +0500

    фильтрация + преобразовние

--- marvel_main.py
+++ marvel_main.py
@@ -2,11 +2,30 @@ from marvel import full_dict
 from pprint import pprint
 
 
-# Шаг 1: Реализуйте ввод от пользователя
+#Реализуйте ввод от пользователя
 user_input = input("Введите ID через пробел: ")
 
-# Шаг 2: Преобразование строки в список чисел, замена нечисловых элементов на None
+#Преобразование строки в список чисел
 ids = list(map(lambda x: int(x) if x.isdigit() else None, user_input.split()))
 
 # Результат обработки списка ID
 print("Список ID после обработки:", ids)
+
+filtered_dict = {
+    key: value for key, value in full_dict.items() if key in ids
+}
+
+# Результат фильтрации
+print("Фильтрованный словарь по ID:")
+pprint(filtered_dict)
+
+#Создайте множество уникальных значений ключа 'director'
+directors_set = {value['director'] for value in full_dict.values() if 'director' in value}
+
+print("Множество режиссёров:")
+print(directors_set)
+
+#Копия словаря, где 'year' преобразован в строку
+transformed_dict = {
+    key: {**value, 'year': str(value['year'])} for key, value in full_dict.items()
+}
